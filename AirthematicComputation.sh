32bc8904ada6ce6f0a9ee9ad6aa002a0be4bd8e5
amtksms97 <amtksms97@gmail.com>
2020-11-04 17:31:22 +0530

    Moved the values from Dictionary to Array

--- AirthematicComputation.sh
+++ AirthematicComputation.sh
@@ -8,3 +8,5 @@ c2=`expr $x \* $y + $z`
 c3=`expr $z + $x / $y`
 c4=`expr $x % $y + $z`
 
+declare -A computation=([comp1]=$c1 [comp2]=$c2 [comp3]=$c3 [comp4]=$c4)
+declare -a array=("${computation[@]}")
