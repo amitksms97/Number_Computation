d524477aa8597a1d4cdbed092422d74b51992934
amtksms97 <amtksms97@gmail.com>
2020-11-05 08:57:34 +0530

    Sorted the result in ascending order

--- AirthematicComputation.sh
+++ AirthematicComputation.sh
@@ -2,11 +2,16 @@
 read -p "Enter 1st number" x
 read -p "Enter 2nd number" y
 read -p "Enter 3rd number" z
-
+echo "$x | $y | $z"
 c1=`expr $x + $y \* $z`
 c2=`expr $x \* $y + $z`
 c3=`expr $z + $x / $y`
 c4=`expr $x % $y + $z`
+declare -A computation
+computation=([comp1]=$c1 [comp2]=$c2 [comp3]=$c3 [comp4]=$c4)
+declare -a array
+array=(${computation[@]})
+echo "Result in Ascending order:"
+n=$(printf '%s\n' "${array[@]}" | sort -n)
+echo $n
 
-declare -A computation=([comp1]=$c1 [comp2]=$c2 [comp3]=$c3 [comp4]=$c4)
-declare -a array=("${computation[@]}")
